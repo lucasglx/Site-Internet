5d0e2b2daa9a144a364b25f4ad2f729dc826c19f
lucasglx <lucas.gallix@gmail.com>
2017-05-12 16:58:55 +0200

    Add files via upload
    Site avec les photos dans le dossier img

---
+++ Site Internet V2/Contact.php
@@ -0,0 +1,47 @@
+<html>
+
+  <head>
+
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="main.css" />
+    <title>Contact</title>
+
+  </head>
+
+    <body>
+        
+        <div class="topnav">
+            <a class="active" href="#fr/en">Fr/En</a>
+            <a href="Page%20d'accueil.php">Accueil</a>
+            <a href="Qui%20sommes%20nous.php">Qui sommes nous ?</a>
+            <a href="Contact.php">Contact</a>
+            <a href="FAQ.php">FAQ</a>
+        </div>
+
+        <div class="contacteznous">
+            <p><img src= "logomysmarthousepetit.png" class="logoentreprisecontact" alt="Logo entreprise"  /></p>
+        
+            <h1>Contactez-nous</h1>
+        </div>
+        <div>
+            <fieldset>
+                
+                <legend>Différents moyens de nous contacter</legend>
+                
+                <p>
+                    <a href="mailto:mysmarthouse.contact@gmail.com">E-mail</a>      
+                </p>
+                
+                <p>     Téléphone :
+                        <span id="clickToShow">01 23 45 67 89</span>
+                </p>
+                
+                <p>
+                        <a href="https://www.facebook.com/My-Smart-House-1021275438007096/?fref=ts" class="button" target="_blank"><img src="img/36cd9861734e0c2a7b39cf1468842845_facebook-logo-facebook-logo-for-website-clipart_864-311.jpeg" alt="Facebook" style="width:80px;height:30px;"></a>
+                </p>
+               
+                </fieldset>
+        </div>        
+        
+    </body>
+</html>
---
+++ Site Internet V2/Qui sommes nous.php
@@ -0,0 +1,26 @@
+<html>
+
+  <head>
+
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="main.css" />
+    <title>Qui sommes nous ?</title>
+
+  </head>
+
+    <body>
+
+        <div class="topnav">
+            <a class="active" href="#fr/en">Fr/En</a>
+            <a href="Page%20d'accueil.php">Accueil</a>
+            <a href="Qui%20sommes%20nous.php">Qui sommes nous ?</a>
+            <a href="Contact.php">Contact</a>
+            <a href="FAQ.php">FAQ</a>
+        </div>
+
+       <div class="bienvenue">
+               <img src= "logomysmarthousepetit.png" class="logoentreprise" alt="Logo entreprise"  />
+            <h1>Présentation de l'entreprise</h1>
+        </div>
+    </body>
+</html>
